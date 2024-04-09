56800ac98e5f1b91c47faaca64a86f2ee355abd9
Gayanath Lakmevan Silva <gayanathlakmevansilva@gmail.com>
2024-04-09 19:07:56 +0530

    add-test

--- src/Test/AddOrUpdateUser.test.js
+++ src/Test/AddOrUpdateUser.test.js
@@ -31,36 +31,36 @@ describe('AddOrUpdateUser Component', () => {
     fireEvent.click(createButton);
 
     
-    // const actions = store.getActions();
+   
     const actions = UserAction;
     expect(actions[0].type).toBe('ADD_USER');
     expect(actions[0].payload.firstName).toBe('John');
     expect(actions[0].payload.lastName).toBe('Doe');
   });
 
-  // test('should update an existing user', () => {
-  //   const existingUser = { id: 1, firstName: 'Jane', lastName: 'Smith' };
-  //   const initialState = { users: [existingUser] };
-  //   store = mockStore(rootReducer(initialState, {}));
+  test('should update an existing user', () => {
+    const existingUser = { id: 1, firstName: 'Jane', lastName: 'Smith' };
+    const initialState = { users: [existingUser] };
+    store = mockStore(rootReducer(initialState, {}));
 
-  //   const { getByLabelText, getByText } = render(
-  //     <Provider store={store}>
-  //       <AddOrUpdateUser user={existingUser} />
-  //     </Provider>
-  //   );
+    const { getByLabelText, getByText } = render(
+      <Provider store={store}>
+        <AddOrUpdateUser user={existingUser} />
+      </Provider>
+    );
 
-  //   const firstNameInput = getByLabelText('First Name');
-  //   const lastNameInput = getByLabelText('Last Name');
-  //   fireEvent.change(firstNameInput, { target: { value: 'Alice' } });
-  //   fireEvent.change(lastNameInput, { target: { value: 'Johnson' } });
+    const firstNameInput = getByLabelText('First Name');
+    const lastNameInput = getByLabelText('Last Name');
+    fireEvent.change(firstNameInput, { target: { value: 'Alice' } });
+    fireEvent.change(lastNameInput, { target: { value: 'Johnson' } });
 
-  //   const updateButton = getByText('Update');
-  //   fireEvent.click(updateButton);
+    const updateButton = getByText('Update');
+    fireEvent.click(updateButton);
 
-  //   const actions = store.getActions();
-  //   expect(actions[0].type).toBe('UPDATE_USER');
-  //   expect(actions[0].payload.id).toBe(1); 
-  //   expect(actions[0].payload.updatedData.firstName).toBe('Alice');
-  //   expect(actions[0].payload.updatedData.lastName).toBe('Johnson');
-  // });
+    const actions = store.getActions();
+    expect(actions[0].type).toBe('UPDATE_USER');
+    expect(actions[0].payload.id).toBe(1); 
+    expect(actions[0].payload.updatedData.firstName).toBe('Alice');
+    expect(actions[0].payload.updatedData.lastName).toBe('Johnson');
+  });
 });
